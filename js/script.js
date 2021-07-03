21e10297c9d40e93e82d4055b1c2eca303287424
shwangb80 <54486677+shwangbo80@users.noreply.github.com>
2021-07-03 01:27:09 -0700

    added heart toggle button

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ const myInput = document.getElementById("myInput")
 // myModal.addEventListener("shown.mdb.modal", () => {
 //     myInput.focus()
 // })
+
+// $(document).ready(function () {
+//     $("#heart").click(function () {
+//         if ($("#heart").hasClass("liked")) {
+//             $("#heart").html('<i class="fa fa-heart-o" aria-hidden="true"></i>')
+//             $("#heart").removeClass("liked")
+//         } else {
+//             $("#heart").html('<i class="fa fa-heart" aria-hidden="true"></i>')
+//             $("#heart").addClass("liked")
+//         }
+//     })
+// })
+
+$(".heart.fa").click(function () {
+    $(this).toggleClass("fa-heart fa-heart-o")
+})
