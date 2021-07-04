529f76c1ecc047a2f31892b7caeb582fde002851
Soo Hwangbo <soohwangbo@Soos-Mac-mini-36.home>
2021-07-04 03:30:15 -0700

    new push test from m1 mac mini

--- js/script.js
+++ js/script.js
@@ -1,22 +1,6 @@
 const myModal = document.getElementById("myModal")
 const myInput = document.getElementById("myInput")
 
-// myModal.addEventListener("shown.mdb.modal", () => {
-//     myInput.focus()
-// })
-
-// $(document).ready(function () {
-//     $("#heart").click(function () {
-//         if ($("#heart").hasClass("liked")) {
-//             $("#heart").html('<i class="fa fa-heart-o" aria-hidden="true"></i>')
-//             $("#heart").removeClass("liked")
-//         } else {
-//             $("#heart").html('<i class="fa fa-heart" aria-hidden="true"></i>')
-//             $("#heart").addClass("liked")
-//         }
-//     })
-// })
-
 $(".heart.fa").click(function () {
     $(this).toggleClass("fa-heart fa-heart-o")
 })
